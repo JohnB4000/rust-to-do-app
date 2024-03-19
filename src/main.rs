bedc6c7bb465ba62fe8527529a1c2a3b99b262e8
John B <jb2069@hw.ac.uk>
2024-03-19 23:38:10 +0000

    Add main loop and input logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::io::{self, Write};
+
+struct ToDoItem {
+    name: String,
+    status: bool,
+    due: String,
+    description: String,
+    subitems: Vec<ToDoItem>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut input = String::new();
+
+        print!(">>> ");
+        io::stdout().flush().expect("Failed to flush stdout!");
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line!");
+
+        let input = input.trim();
+
+        match input {
+            "exit" => break,
+            _ => (),
+        }
+
+        println!("You entered: {}", input);
+    }
+
+    println!("Thanks for using the To Do App");
 }
